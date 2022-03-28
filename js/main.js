fddd801bb7e672ceb11e7f6cc3d2f01d091ab111
Your Name <thomas.sanichchung@gmail.com>
2022-03-28 17:08:18 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,5 @@
+const btn1 = document.getElementById('btn1');
+const btn2 = document.getElementById('btn2');
+
+btn1.onclick = () => document.body.style.backgroundColor = "#2F2F2F";
+btn2.onclick = () => document.body.style.backgroundColor = "#dadada";
